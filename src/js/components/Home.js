c767b3a58945426412c92dcb43a7a5b2018c995f
Bartłomiej Szczotkowski <szczotkowski.b@gmail.com>
2022-01-03 22:04:49 +0100

    Add page activation for links in Home.js

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,4 +1,4 @@
-import { templates,} from '../settings.js';
+import { select, templates, classNames} from '../settings.js';
 
 
 class Home{
@@ -6,6 +6,7 @@ class Home{
     const thisHome = this;
 
     thisHome.render(element);
+    thisHome.initPage();
   }
 
   render(element){
@@ -16,6 +17,40 @@ class Home{
     thisHome.dom.wrapper = element;
     thisHome.dom.wrapper.innerHTML = generatedHTML;
 
+  }
+
+  activatePage(pageId){
+    const thisHome = this;
+
+    thisHome.pages = document.querySelector(select.containerOf.pages).children;
+    thisHome.navLinks = document.querySelectorAll(select.nav.links);
+
+    for(let page of thisHome.pages){
+      page.classList.toggle(classNames.pages.active, page.id === pageId);
+    }
+
+    for(let link of thisHome.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') === '#' + pageId
+      );
+    }
+  }
+
+  initPage(){
+    const thisHome = this;
+
+    thisHome.links = document.querySelectorAll('.link');
+
+    for(let link of thisHome.links){
+      link.addEventListener('click', function(event){
+        event.preventDefault;
+        const clickedLink = this;
+        const id = clickedLink.getAttribute('href').replace('#', '');
+
+        thisHome.activatePage(id);
+      });
+    }
   }
 }
 
